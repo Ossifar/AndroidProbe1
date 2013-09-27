119a6b5e2ceb7842780a42400fee9b136aff8b1b
Rafel Payeras Roig <rafel.payeras.roig@gmail.com>
2013-09-27 18:29:18 +0200

    Add buttons and methods to change activities.

--- AndroidProbe1/src/main/java/com/bubographics/androidprobe1/SecondActivity.java
+++ AndroidProbe1/src/main/java/com/bubographics/androidprobe1/SecondActivity.java
@@ -6,24 +6,23 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class SecondActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 
-    public void GotoPantalla2(View view) {
+    public void GotoPantalla1 (View view) {
         Intent i = new Intent(this, SecondActivity.class);
         startActivity(i);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.second, menu);
         return true;
     }
     
